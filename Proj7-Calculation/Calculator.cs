3692b294c824463c7b983a656429d3e3552bb1b5
Scotty Snyder <SNYDERDS@ETSU.EDU>
2023-04-30 08:32:30 -0400

    Add project files.

---
+++ Proj7-Calculation/Calculator.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj7_Calculation
+{
+    public class Calculator
+    {
+        Dictionary<string, double> CalcInput = new();
+
+        public double GetValue()
+        {
+            return CalcInput.ContainsKey("currentValue") ? CalcInput["currentValue"] : 0;
+        }
+
+        public void Store(string key, string value)
+        {
+            double x;
+            double.TryParse(value, out x);
+            CalcInput[key] = x;
+        }
+
+        public double Parse(string value)
+        {
+            double x;
+            if(CalcInput.ContainsKey(value))
+            {
+                x = CalcInput[value];
+            }
+            else
+            {
+                double.TryParse(value, out x);
+            }
+            return x;
+        }
+
+        public void Add(string a, string b)
+        {
+            double x = Parse(a);
+            double y = Parse(b);
+            CalcInput["currentValue"] = x + y;
+        }
+
+        public void Sub(string a, string b)
+        {
+            double x = Parse(a);
+            double y = Parse(b);
+            CalcInput["currentValue"] = y - x;
+        }
+
+        public void Mult(string a, string b)
+        {
+            double x = Parse(a);
+            double y = Parse(b);
+            CalcInput["currentValue"] = x * y;
+        }
+
+        public void Div(string a, string b)
+        {
+            double x = Parse(a);
+            double y = Parse(b);
+            CalcInput["currentValue"] = x / y;
+        }
+
+        public void Sqr(string a)
+        {
+            double x = Parse(a);
+            CalcInput["currentValue"] = x * x;
+        }
+
+        public void Sqrt(string a)
+        {
+            double x = Parse(a);
+            CalcInput["currentValue"] = Math.Sqrt(x);
+        }
+
+        public void Clear(string a)
+        {
+            double x = Parse(a);
+            CalcInput["currentValue"] = 0;
+        }
+
+        public void Pow(string a, string b)
+        {
+            double x = Parse(a);
+            double y = Parse(b);
+            CalcInput["currentValue"] = Math.Pow(x, y);
+        }
+
+        public void Mod(string a, string b)
+        {
+            double x = Parse(a);
+            double y = Parse(b);
+            CalcInput["currentValue"] = x % y;
+        }
+
+        public void Fact(string a)
+        {
+            double x = Parse(a);
+            double temp = x;
+
+            for(double i = x - 1; i > 0; i--)
+            {
+                temp *= i;
+            }
+
+            CalcInput["currentValue"] = temp;
+        }
+    }
+}
---
+++ Proj7-Calculation/Driver.cs
@@ -0,0 +1,55 @@
+﻿namespace Proj7_Calculation
+{
+    internal class Driver
+    {
+        static void Main()
+        {
+            Calculator calc = new Calculator();
+
+            Dictionary<string, dynamic> dispatch = new();
+            dispatch["add"] = new Action<string, string>((a, b) => calc.Add(a, b));
+            dispatch["store"] = new Action<string, string>((a, b) => calc.Store(a, b));
+            dispatch["sub"] = new Action<string, string>((a, b) => calc.Sub(a, b));
+            dispatch["mult"] = new Action<string, string>((a, b) => calc.Mult(a, b));
+            dispatch["div"] = new Action<string, string>((a, b) => calc.Div(a, b));
+            dispatch["pow"] = new Action<string, string>((a, b) => calc.Pow(a, b));
+            dispatch["mod"] = new Action<string, string>((a, b) => calc.Mod(a, b));
+            dispatch["fact"] = new Action<string>((a) => calc.Fact(a));
+            dispatch["sqr"] = new Action<string>((a) => calc.Sqr(a));
+            dispatch["sqrt"] = new Action<string>((a) => calc.Sqrt(a));
+            dispatch["clear"] = new Action<string>((a) => calc.Clear(a));
+
+
+            while(true)
+            {
+                Console.WriteLine(calc.GetValue());
+                string[] input = Console.ReadLine().ToLower().Split(" ");
+
+                if(input.Length == 3)
+                {
+                    dispatch[input[0]](input[1], input[2]);
+                }
+                else if(input.Length == 2)
+                {
+                    if(input[0] == "div" || input[0] == "pow" || input[0] == "mod")
+                    {
+                        dispatch[input[0]](calc.GetValue().ToString(), input[1]);
+                    }
+                    else if(input[0] == "sqr" || input[0] == "sqrt" || input[0] == "fact")
+                    {
+                        dispatch[input[0]](input[1]);
+                    }
+                    else
+                    {
+                        dispatch[input[0]](input[1], calc.GetValue().ToString());
+                    }
+                }
+                else if (input.Length == 1)
+                {
+                    dispatch[input[0]](calc.GetValue().ToString());
+                }
+
+            }
+        }
+    }
+}
